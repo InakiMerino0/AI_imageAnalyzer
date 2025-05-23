92c04ee240071cf05791da78d7daddfdded17107
InakiMerino0 <inakimerino2308@gmail.com>
2025-05-23 20:54:08 -0300

    main ui added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "appwidget.h" // Nuestro widget principal
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv); // Objeto de aplicación Qt
+
+    QFile styleFile(":/styles.qss");  // Importante: el ":" indica recurso embebido
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString styleSheet = QLatin1String(styleFile.readAll());
+        qApp->setStyleSheet(styleSheet);
+    }
 
     AppWidget w; // Creamos una instancia de nuestro AppWidget
     w.show();    // Mostramos la ventana
